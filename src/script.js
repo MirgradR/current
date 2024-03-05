0bbe02465912accd786458b789faf61ff2e45e7e
Oknemzuk <vvkuzmenk@gmail.com>
2024-03-06 00:59:23 +0300

    RF-33, change sector welcome. +deleted sectors: about and contacts

--- src/script.js
+++ src/script.js
@@ -3,14 +3,9 @@ import header from "./components/header/header.html";
 import mainContent from "./components/main/main.html";
 import section from "./components/section/section.html";
 import welcome from "./components/welcome/welcome.html";
-import about from "./components/about/about.html";
-import contacts from "./components/contacts/contacts.html";
-
 
 import "./components/header/header.css";
 import "./components/welcome/welcome.css";
-import "./components/about/about.css";
-import "./components/contacts/contacts.css";
 
 // Слушатель события, который выполнится, когда весь DOM загрузится
 document.addEventListener("DOMContentLoaded", async function () {
