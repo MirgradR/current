2776550da9c5c9af7ad0ae44681923d9351f1fb7
Oknemzuk <160840889+Oknemzuk@users.noreply.github.com>
2024-03-12 16:06:09 +0300

    RF-61 add footer, and maked little work on mistake throw all work (#20)

--- src/script.js
+++ src/script.js
@@ -5,26 +5,28 @@ import mainContent from "./components/main/main.html";
 import banner from "./components/banner/banner.html";
 import presentation from "./components/presentation/presentation.html";
 import partners from "./components/partners/partners.html";
-import integrates from "./components/integrates/integrates.html";
 import offer from "./components/offer/offer.html";
+import integrates from "./components/integrates/integrates.html";
 import summarized from "./components/summarized/summarized.html";
 import weekly_drop from "./components/weekly_drop/weekly_drop.html";
 import features from "./components/features/features.html";
 import conditions from "./components/conditions/conditions.html";
 import appear from "./components/appear/appear.html";
+import footer from "./components/footer/footer.html";
 
 import "../styles.css";
 import "./components/header/header.css";
 import "./components/banner/banner.css";
 import "./components/presentation/presentation.css";
 import "./components/partners/partners.css";
-import "./components/integrates/integrates.css";
 import "./components/offer/offer.css";
+import "./components/integrates/integrates.css";
 import "./components/summarized/summarized.css";
 import "./components/weekly_drop/weekly_drop.css";
 import "./components/features/features.css";
 import "./components/conditions/conditions.css";
 import "./components/appear/appear.css";
+import "./components/footer/footer.css";
 
 // Слушатель события, который выполнится, когда весь DOM загрузится
 document.addEventListener("DOMContentLoaded", async function () {
@@ -39,13 +41,14 @@ document.addEventListener("DOMContentLoaded", async function () {
   await insertHTMLIntoElementById("main", banner);
   await insertHTMLIntoElementById("main", partners);
   await insertHTMLIntoElementById("main", presentation);
-  await insertHTMLIntoElementById("main", integrates);
   await insertHTMLIntoElementById("main", offer);
+  await insertHTMLIntoElementById("main", integrates);
   await insertHTMLIntoElementById("main", summarized);
   await insertHTMLIntoElementById("main", weekly_drop);
   await insertHTMLIntoElementById("main", features);
   await insertHTMLIntoElementById("main", conditions);
   await insertHTMLIntoElementById("main", appear);
+  await insertHTMLIntoElementById("main", footer);
 });
 
 // Функция для вставки HTML-контента в элемент с заданным ID
